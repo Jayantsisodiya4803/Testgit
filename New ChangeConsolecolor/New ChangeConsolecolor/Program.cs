56ef1f7a29d9fffa34ba42490b73a9404dc0e080
jayantsisodiya4803 <jayant.sisodiya@accenture.com>
2020-03-26 21:12:54 +0530

    C# CODE

---
+++ New ChangeConsolecolor/New ChangeConsolecolor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_ChangeConsolecolor
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("I heloo word");
+            Console.WriteLine("wassup dude");
+            Console.WriteLine("wassup dude1");
+            Console.Read();
+            
+        }
+    }
+}
